640a7516d7f148b23a9b195c4c0a7f01f310feac
hanley-dev <96717063+hanley-dev@users.noreply.github.com>
2021-12-30 20:20:40 -0800

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,15 @@
 <?php
 echo "Hello world" . PHP_EOL;
 
+$date = new DateTime();
+$tz = $date->getTimezone();
+
+echo "Time zone: " . $tz->getName() . PHP_EOL;
+echo PHP_EOL;
+
 $env_array = getenv();
 
-echo "The list of environment variables with values are :" . PHP_EOL;
+echo "The list of environment variables with values are:" . PHP_EOL;
 
 foreach ($env_array as $key=>$value)
 {
