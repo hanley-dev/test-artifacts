7cecf0401b4b6fac22ea1bee2c0b9ef7740699ed
hanley-dev <96717063+hanley-dev@users.noreply.github.com>
2021-12-30 20:14:24 -0800

    Update test.php

--- test.php
+++ test.php
@@ -1,2 +1,11 @@
 <?php
 echo "Hello world" . PHP_EOL;
+
+$env_array = getenv();
+
+echo "The list of environment variables with values are :" . PHP_EOL;
+
+foreach ($env_array as $key=>$value)
+{
+    echo "$key => $value" . PHP_EOL;
+}
